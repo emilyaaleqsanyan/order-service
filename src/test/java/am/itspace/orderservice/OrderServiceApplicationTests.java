49d49c218e9b5a914c063aa90bd4919145b375b4
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2024-04-27 22:06:34 +0400

    Add new file

---
+++ src/test/java/am/itspace/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
